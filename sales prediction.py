da6c80fb2f5c953ef82582858d947d5828ed0734
gbolahanolumade <olumadegbolahan@gmail.com>
2021-01-01 23:58:41 +0100

    Outlier Check

--- sales prediction.py
+++ sales prediction.py
@@ -5,3 +5,39 @@ Created on Fri Jan  1 23:26:54 2021
 @author: GbolahanOlumade
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("advertising.csv")
+
+df.shape
+df.info()
+df.describe()
+
+
+df(df.isnull())
+
+df.isnull().sum()*100/df.shape[0]
+
+df.isnull().sum()
+
+
+
+plt.title("Outlier Search")
+plt.xlabel("advert")
+plt.ylabel("amount spent")
+plt.boxplot(df)
+plt.show()
+
+
+
+
+
+
+fig, axs = plt.subplots(3, figsize = (5,5))
+plt1 = sns.boxplot(df['TV'], ax = axs[0])
+plt2 = sns.boxplot(df['Newspaper'], ax = axs[1])
+plt3 = sns.boxplot(df['Radio'], ax = axs[2])
+plt.tight_layout()
